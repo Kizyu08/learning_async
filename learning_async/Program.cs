ef92c41f940ab1de59f8a49823451a2348abe39e
nossan1609 <forutexx08@gmail.com>
2020-01-22 16:21:39 +0900

    first

---
+++ learning_async/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace learning_async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stopwatchクラス生成
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            //Console.WriteLine("Hello World!");
+            int i = Calculate();
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+
+        static int Calculate()
+        {
+            int total = 0;
+            for (int i = 1; i <= 100; ++i)
+                total += i;
+            Thread.Sleep(2500); // 何か重い処理をしている...
+            return total;
+        }
+    }
+}
